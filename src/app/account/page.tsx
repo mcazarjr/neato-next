de9d963a6a4c98828a4ba8e8227c30224818f4b0
Meraldo Cazar <mscj@Meraldos-MacBook-Pro.local>
2025-01-19 22:47:03 -0800

    init/simple app navigation

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,20 @@
+import Link from 'next/link';
+
 const pages = () => {
-    return <div>dashboard</div>;
+    return (
+        <div>
+            <h1>Account</h1>
+            <Link href='/'>Request</Link>
+            <br />
+            <Link href='/account/medication'>Medication</Link>
+            <br />
+            <Link href='/account/prescription'>Prescription</Link>
+            <br />
+            <Link href='/account/profile'>Profile</Link>
+            <br />
+            <Link href='/account/settings'>Settings</Link>
+        </div>
+    );
 };
 
 export default pages;
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
 const pages = () => {
-    return <div>signin</div>;
+    return (
+        <div>
+            <h1>Sign In</h1>
+            <Link href='/'>Home</Link>
+            <Link href='/signup'>Sign Up</Link>
+        </div>
+    );
 };
 
 export default pages;
